1c481600214c4de777b636f03e26daefca7ef39c
santisfdez <101612992+santisfdez@users.noreply.github.com>
2022-05-06 14:05:35 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-SELECT avg(c21), stfrtime("%Y",c23) FROM tbl2 GROUP BY stfrtime("%Y",c23);
+SELECT avg(c21), strftime("%Y",c23) FROM tbl2 GROUP BY strftime("%Y",c23);
